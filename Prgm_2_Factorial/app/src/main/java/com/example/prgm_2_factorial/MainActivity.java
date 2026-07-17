c289da1328164abea930855e566a5e70bbe96734
JiphinGeorge <jiphingeorge80.com>
2026-07-17 11:34:53 +0530

    .

--- Prgm_2_Factorial/app/src/main/java/com/example/prgm_2_factorial/MainActivity.java
+++ Prgm_2_Factorial/app/src/main/java/com/example/prgm_2_factorial/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.prgm_2_factorial;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editText = findViewById(R.id.editTextText);
+        textView = findViewById(R.id.textView);
+
+        button = new Button(this);
+        button.setText("Calculate");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int num = Integer.parseInt(editText.getText().toString());
+                long fact = 1;
+
+                for (int i = 1; i <= num; i++) {
+                    fact = fact * i;
+                }
+
+                textView.setText("Factorial = " + fact);
+            }
         });
     }
 }
