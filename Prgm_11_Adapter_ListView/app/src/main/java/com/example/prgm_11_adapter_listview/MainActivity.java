1bf16363c4b428b2d4a6c49fe4240cd6978f8c73
JiphinGeorge <jiphingeorge80.com>
2026-09-11 11:44:27 +0530

    Add Lab Program 9

--- Prgm_11_Adapter_ListView/app/src/main/java/com/example/prgm_11_adapter_listview/MainActivity.java
+++ Prgm_11_Adapter_ListView/app/src/main/java/com/example/prgm_11_adapter_listview/MainActivity.java
@@ -4,42 +4,18 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
-    String[] days = {
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday"
-    };
+    ListView list;
+    String []days= {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        listView = findViewById(R.id.main);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                days
-        );
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener((parent, view, i, id) -> {
-
-            Toast.makeText(
-                    MainActivity.this,days[i] +" "+
-                    + i,
-                    Toast.LENGTH_SHORT
-            ).show();
-
-        });
+        list=findViewById(R.id.main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,days );
+        list.setAdapter(adapter);
+        list.setOnItemClickListener((parent, view, position, id) ->
+                Toast.makeText(MainActivity.this,days[position] +" "+position,Toast.LENGTH_SHORT ).show());
     }
 }
